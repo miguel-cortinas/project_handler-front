c84824aa917edd1d65dedeb558db9431854c9039
miguel-cortinas <118024590+miguel-cortinas@users.noreply.github.com>
2023-11-25 14:41:37 -0600

    add docs

---
+++ docs/precache-manifest.60075f92b2a15e0247edc9d3c02e72c7.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c70dd57d64cf2c9e08f",
+    "url": "/css/app.f7c92974.css"
+  },
+  {
+    "revision": "2c70dd57d64cf2c9e08f",
+    "url": "/js/app.fb1f05e1.js"
+  },
+  {
+    "revision": "e137a960b27327ed593f",
+    "url": "/css/chunk-vendors.749d2acd.css"
+  },
+  {
+    "revision": "e137a960b27327ed593f",
+    "url": "/js/chunk-vendors.7f1ab9c1.js"
+  },
+  {
+    "revision": "20206738b2bffb741b00200d5d3d6d20",
+    "url": "/fonts/fa-regular-400.20206738.ttf"
+  },
+  {
+    "revision": "8b0ddedbb27cbc9971c8667caa8a0cc1",
+    "url": "/fonts/fa-brands-400.8b0ddedb.woff2"
+  },
+  {
+    "revision": "e2ceb83946c9e5fc7eab24453a03bffb",
+    "url": "/fonts/fa-solid-900.e2ceb839.ttf"
+  },
+  {
+    "revision": "61f30b79daf5b31f0d254a31fba66158",
+    "url": "/fonts/fa-regular-400.61f30b79.woff2"
+  },
+  {
+    "revision": "4bc58bc16bb05a05d3a47a4f7e143b75",
+    "url": "/fonts/fa-v4compatibility.4bc58bc1.woff2"
+  },
+  {
+    "revision": "d20cedd7e254d4b58b721b6995ca52b4",
+    "url": "/fonts/fa-v4compatibility.d20cedd7.ttf"
+  },
+  {
+    "revision": "c64278386c2bbb5e293e11b94ca2f6d1",
+    "url": "/fonts/fa-solid-900.c6427838.woff2"
+  },
+  {
+    "revision": "0ab3921d9b80975c5597432ab59f5d0a",
+    "url": "/fonts/fa-brands-400.0ab3921d.ttf"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "8c6fc28b1a644530e0589ad0aa6e1e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.gitkeep"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "be74132f42ae1d3502f9a9b9fff68ac1",
+    "url": "/Dashboard.PNG"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "99e58416b89637502b40ac8350eed85a",
+    "url": "/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/img/faces/face-7.jpg"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/img/faces/face-6.jpg"
+  },
+  {
+    "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
+    "url": "/img/faces/face-4.jpg"
+  },
+  {
+    "revision": "43b98081492ac3bcb4a1fac6cf709403",
+    "url": "/img/faces/tim_vector.jpe"
+  },
+  {
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "d27fbc90c2e644dfdc9765640dc713b9",
+    "url": "/img/mask.png"
+  },
+  {
+    "revision": "f575a04ebbb31b5798a4c54783e745a2",
+    "url": "/img/new_logo.png"
+  },
+  {
+    "revision": "0a337c37f03c0462996b7b03758fa72b",
+    "url": "/img/loading-bubbles.svg"
+  },
+  {
+    "revision": "cd253e23ed052deeb80b42d2ed772183",
+    "url": "/img/sidebar-3.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/img/sidebar-2.jpg"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/img/sidebar-4.jpg"
+  },
+  {
+    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
+    "url": "/img/vue-logo.png"
+  },
+  {
+    "revision": "44bf13a71a4db6e15913fe8af9296711",
+    "url": "/img/tim_80x80.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/img/sidebar-1.jpg"
+  }
+];
